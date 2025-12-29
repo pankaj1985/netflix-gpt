e2e8604ebe59f872c10ae016dd9f9bd302e559c6
root <root@LAPTOP-PHGFEHFR.localdomain>
2025-12-29 10:37:51 +0000

    Updated project: Add tailwind config and postcss config file and other updated files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="font-bold text-green-600 text-3xl">Netflix GPT Project</div>
     </div>
   );
 }
